cad687a131c2a63e27c5bca93356e51fa64a76c9
josebatista2 <josebatistajosevitor@outlook.com>
2022-03-17 15:30:03 -0300

    Calculo de area dos tres Quadrilateros

---
+++ src/Dio/projeto1/MMain.java
@@ -0,0 +1,10 @@
+package Dio.projeto1;
+
+public class MMain {
+    public static void main(String[] args) {
+        System.out.println("exercicio quadrilatero");
+        Quadrilatero.area(4);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+    }
+}
