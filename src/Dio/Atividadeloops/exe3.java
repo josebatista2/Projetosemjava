d7207e2d6c063f3ccee0c8a1bb3b0fa46035386b
josebatista2 <josebatistajosevitor@outlook.com>
2022-03-23 17:35:04 -0300

    Maior , media , soma .

---
+++ src/Dio/Atividadeloops/exe3.java
@@ -0,0 +1,24 @@
+package Dio.Atividadeloops;
+
+import java.util.Scanner;
+
+public class exe3 {
+    public static void main(String[] args) {
+        Scanner scan = new  Scanner(System.in);
+
+        int numero ;
+        int count = 0;
+        int maior =0 ;
+        int soma = 0 ;
+        do {
+            System.out.println("Numero :");
+            numero = scan.nextInt();
+            soma = soma + numero ;
+            if (numero > maior )maior = numero ;
+            count = count + 1 ;
+
+        }while (count < 5 );
+            System.out.println("Maior : " + maior );
+            System.out.println("Media : " + (soma/5 ) );
+    }
+}
